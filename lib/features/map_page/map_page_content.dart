4a93b4126e0d8eb4038118bd13ea3b48006cf033
Jakub Smolik <smolikja@protonmail.com>
2025-11-06 17:22:28 +0100

    feat: map search migrated to map page

--- lib/features/map_page/map_page_content.dart
+++ lib/features/map_page/map_page_content.dart
@@ -4,12 +4,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:studanky_flutter_app/features/map_page/entities/map_search_result.dart';
 import 'package:studanky_flutter_app/features/map_page/map_page_constants.dart';
 import 'package:studanky_flutter_app/features/map_page/providers/map_marker_provider.dart';
+import 'package:studanky_flutter_app/features/map_page/providers/map_search_provider.dart';
+import 'package:studanky_flutter_app/features/map_page/widgets/map_search_overlay.dart';
 import 'package:studanky_flutter_app/features/map_page/widgets/marker.dart';
-import 'package:studanky_flutter_app/features/map_search/models/map_search_result.dart';
-import 'package:studanky_flutter_app/features/map_search/providers/map_search_providers.dart';
-import 'package:studanky_flutter_app/features/map_search/widgets/map_search_overlay.dart';
 
 class MapPageContent extends ConsumerStatefulWidget {
   const MapPageContent({super.key});
@@ -22,7 +22,7 @@ class _MapPageContentState extends ConsumerState<MapPageContent> {
   static const LatLng _initialCenter = LatLng(
     49.5630,
     15.9398,
-  ); // TODO: ask user's for location
+  ); // TODO: ask for user's location
   static const double _defaultZoom = 14.5;
 
   final MapController _mapController = MapController();
