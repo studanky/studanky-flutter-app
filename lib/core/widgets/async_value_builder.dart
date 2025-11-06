08904adf43828516662c32d2870652a80b8a1a4e
Jakub Smolik <smolikja@protonmail.com>
2025-11-06 13:26:08 +0100

    feat: async value builder

---
+++ lib/core/widgets/async_value_builder.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:studanky_flutter_app/core/widgets/app_progress_indicator.dart';
+import 'package:studanky_flutter_app/core/widgets/error_widget.dart';
+
+class AsyncValueBuilder<T> extends StatelessWidget {
+  const AsyncValueBuilder({
+    super.key,
+    required this.asyncValue,
+    required this.data,
+    this.error,
+    this.loading,
+    this.onRefresh,
+  });
+
+  final AsyncValue<T> asyncValue;
+  final Widget Function(T data) data;
+  final Widget Function(Object error, StackTrace stack)? error;
+  final Widget Function()? loading;
+  final VoidCallback? onRefresh;
+
+  static Widget _defaultLoading() {
+    return const Center(child: AppProgressIndicator());
+  }
+
+  Widget _defaultError(Object err, StackTrace stack) {
+    return AppErrorWidget(onRefresh: onRefresh);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return asyncValue.when(
+      data: data,
+      error: error ?? _defaultError,
+      loading: loading ?? _defaultLoading,
+    );
+  }
+}
