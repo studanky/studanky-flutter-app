85824d1479e6b5fa9b26587f455b341cd8b7b75d
Jakub Smolik <smolikja@protonmail.com>
2025-11-10 11:41:53 +0100

    feat: map suggest query bo annoted

--- lib/features/map_search/bos/map_suggest_query_bo.dart
+++ lib/features/map_search/bos/map_suggest_query_bo.dart
@@ -1,3 +1,8 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'map_suggest_query_bo.g.dart';
+
+@JsonSerializable(createFactory: false)
 class MapySuggestQueryBO {
   const MapySuggestQueryBO({
     required this.query,
@@ -6,18 +11,25 @@ class MapySuggestQueryBO {
     required this.types,
   });
 
+  /// User-entered text.
   final String query;
+
+  /// Expected localization returned by the API.
+  @JsonKey(name: 'lang')
   final String language;
+
+  /// Number of suggestions requested.
   final int limit;
+
+  /// Mapy.cz expects `type=a,b,c` instead of an array.
+  @JsonKey(name: 'type', toJson: _typesToCsv)
   final List<String> types;
 
+  Map<String, dynamic> toJson() => _$MapySuggestQueryBOToJson(this);
+
   Map<String, dynamic> toQueryParameters(String apiKey) {
-    return <String, dynamic>{
-      'lang': language,
-      'limit': limit,
-      'type': types.join(','),
-      'apikey': apiKey,
-      'query': query,
-    };
+    return <String, dynamic>{...toJson(), 'apikey': apiKey};
   }
+
+  static String _typesToCsv(List<String> values) => values.join(',');
 }
---
+++ lib/features/map_search/bos/map_suggest_query_bo.g.dart
@@ -0,0 +1,15 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'map_suggest_query_bo.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Map<String, dynamic> _$MapySuggestQueryBOToJson(MapySuggestQueryBO instance) =>
+    <String, dynamic>{
+      'query': instance.query,
+      'lang': instance.language,
+      'limit': instance.limit,
+      'type': MapySuggestQueryBO._typesToCsv(instance.types),
+    };
