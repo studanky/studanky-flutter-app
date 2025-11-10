f23f45f45bbc2c1400d30b3f80c6b1c4cee529cf
Jakub Smolik <smolikja@protonmail.com>
2025-11-10 11:18:02 +0100

    feat: logging init

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:logging/logging.dart';
 import 'package:studanky_flutter_app/core/navigation/app_router.dart';
 import 'package:studanky_flutter_app/l10n/app_localizations.dart';
 
+void _setLogging() {
+  Logger.root.level = Level.ALL; // defaults to Level.INFO
+  Logger.root.onRecord.listen((record) {
+    debugPrint('${record.level.name}: ${record.time}: ${record.message}');
+  });
+}
+
 void main() {
+  _setLogging();
   runApp(const ProviderScope(child: MainApp()));
 }
 
